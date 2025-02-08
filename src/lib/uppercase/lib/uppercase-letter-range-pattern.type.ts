b3f955e9ad2c79429278053abcb43412f1c9fb29
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-02-08 13:13:43 +0000

    refactor(UppercaseLetterRangePattern): use `Quantifier`.

--- src/lib/uppercase/lib/uppercase-letter-range-pattern.type.ts
+++ src/lib/uppercase/lib/uppercase-letter-range-pattern.type.ts
@@ -1,4 +1,5 @@
 // Type.
+import { Quantifier } from '../../quantifier.type';
 import { RegExpFlag } from '../../flag/lib/regexp-flag.type';
 import { RegExpPattern } from '../../regexp-pattern.type';
 import { UppercaseLetter } from '@typedly/letter';
@@ -10,7 +11,7 @@ import { UppercaseLetterRange } from './uppercase-letter-range.type';
  * @template {UppercaseLetter} [To='Z'] - The ending letter in the range, defaults to 'Z'.
  * @template {string} [Character=''] - An optional string of additional characters that will be escaped and appended to the range.
  * @template {boolean} [Negated=false] - A flag that indicates if the character class should be negated. If true, a caret (^) is added immediately after the opening bracket.
- * @template {number | '' | '*' | '+' | '?'} [Min=''] - The minimum repetition count (optional).
+ * @template {number | '' | Quantifier} [Min=''] - The minimum repetition count (optional).
  * @template {number | ''} [Max=''] - The maximum repetition count (optional).
  * @template {RegExpFlag[]} [Flags=[]] 
  */
@@ -19,7 +20,7 @@ export type UppercaseLetterRangePattern<
   To extends UppercaseLetter = 'Z',
   Character extends string = '',
   Negated extends boolean = false,
-  Min extends number | '' | '*' | '+' | '?' = '',
+  Min extends number | '' | Quantifier = '',
   Max extends number | '' = '',
   Flags extends RegExpFlag[] = []
 > = RegExpPattern<UppercaseLetterRange<From, To, Character, Negated, Min, Max>, Flags>;
