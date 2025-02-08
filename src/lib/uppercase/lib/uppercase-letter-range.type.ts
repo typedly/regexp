c576fd16b9c2b8d46b2b406818eb6df15258583e
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-02-08 13:13:25 +0000

    refactor(UppercaseLetterRange): use `Quantifier`.

--- src/lib/uppercase/lib/uppercase-letter-range.type.ts
+++ src/lib/uppercase/lib/uppercase-letter-range.type.ts
@@ -1,5 +1,6 @@
 // Type.
 import { LetterRange } from '../../letter/lib/letter-range.type';
+import { Quantifier } from '../../quantifier.type';
 import { Repetition } from '../../repetition.type';
 import { UppercaseLetter } from '@typedly/letter';
 /**
@@ -9,7 +10,7 @@ import { UppercaseLetter } from '@typedly/letter';
  * @template {UppercaseLetter} [To='Z'] - The ending letter in the range, defaults to 'Z'.
  * @template {string} [Character=''] - An optional string of additional characters that will be escaped and appended to the range.
  * @template {boolean} [Negated=false] - A flag that indicates if the character class should be negated. If true, a caret (^) is added immediately after the opening bracket.
- * @template {number | '' | '*' | '+' | '?'} [Min=''] - The minimum repetition count (optional).
+ * @template {number | '' | Quantifier} [Min=''] - The minimum repetition count (optional).
  * @template {number | ''} [Max=''] - The maximum repetition count (optional).
  */
 export type UppercaseLetterRange<
@@ -17,6 +18,6 @@ export type UppercaseLetterRange<
   To extends UppercaseLetter = 'Z',
   Character extends string = '',
   Negated extends boolean = false,
-  Min extends number | '' | '*' | '+' | '?' = '',
+  Min extends number | '' | Quantifier = '',
   Max extends number | '' = ''
 > = `${LetterRange<From, To, Character, Negated> }${Repetition<Min, Max>}`;
