9a857dc8bfa0c88a692894f72e3f9d0b28dfde0d
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-02-08 09:33:28 +0000

    refactor(`LetterRangeRepetitionPattern`): constrain with alphabetic letter and use `RegExpPattern`.

--- src/lib/letter/lib/letter-range-repetition-pattern.type.ts
+++ src/lib/letter/lib/letter-range-repetition-pattern.type.ts
@@ -1,6 +1,8 @@
 // Type.
+import { AlphabeticLetter } from '@typedly/letter';
 import { LetterRangeRepetition } from './letter-range-repetition.type';
-import { FlagString, RegExpFlag } from '../../flag';
+import { RegExpFlag } from '../../flag';
+import { RegExpPattern } from '../../regexp-pattern.type';
 /**
  * @description The representation of a letter range with optional quantity constraints, suitable for use in `RegExp` patterns.
  * @example
@@ -8,8 +10,8 @@ import { FlagString, RegExpFlag } from '../../flag';
  * type LettersAndDigits_2to4 = LetterRangeRepetition<'a', 'z', 'd', false, 2, 4>; // Output: "[a-z\\d]{2,4}"
  * type NotUppercase_Exactly5 = LetterRangeRepetition<'A', 'Z', '', true, 5>; // Output: "[^A-Z]{5}"
  * @export
- * @template {string} [From='a'] - The starting letter in the range, defaults to 'a'.
- * @template {string} [To='z'] - The ending letter in the range, defaults to 'z'.
+ * @template {AlphabeticLetter} [From='a'] - The starting letter in the range, defaults to 'a'.
+ * @template {AlphabeticLetter} [To='z'] - The ending letter in the range, defaults to 'z'.
  * @template {string} [Character=''] - An optional string of additional characters that will be escaped and appended to the range.
  * @template {boolean} [Negated=false] - A flag that indicates if the character class should be negated. If true, a caret (^) is added immediately after the opening bracket.
  * @template {number | '' | '*' | '+' | '?'} [Min=''] - The minimum repetition count (optional).
@@ -17,11 +19,11 @@ import { FlagString, RegExpFlag } from '../../flag';
  * @template {RegExpFlag[]} [Flags=[]] 
  */
 export type LetterRangeRepetitionPattern<
-  From extends string = 'a',
-  To extends string = 'z',
+  From extends AlphabeticLetter = 'a',
+  To extends AlphabeticLetter = 'z',
   Character extends string = '',
   Negated extends boolean = false,
   Min extends number | '' | '*' | '+' | '?' = '',
   Max extends number | '' = '',
   Flags extends RegExpFlag[] = []
-> = `/${LetterRangeRepetition<From, To, Character, Negated, Min, Max>}/${FlagString<Flags>}`;
+> = RegExpPattern<LetterRangeRepetition<From, To, Character, Negated, Min, Max>, Flags>;
