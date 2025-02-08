ecc1f60aa950b754d044ef0152d9db0cb79673c0
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-02-08 08:52:50 +0000

    chore(index): update.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,29 +1,36 @@
 export type { Escaped } from './escaped.type';
+export type { FromTo } from './from-to.type';
 
 // Flag.
-export type { Flag } from './flag.type';
-export type { IncludeFlag } from './include-flag.type';
-export type { RegExpFlag } from './regexp-flag.type';
-export type { PatternFlag } from './pattern-flag.type';
+export type {
+  FlagString,
+  IncludeFlag,
+  IncludeFlags,
+  RegExpFlag,
+} from './flag';
 
 // Repetition.
-export type { Repetition } from './repetition.type';
+export type {
+  Repetition
+} from './repetition.type';
 
 // Letter range.
-export type { LetterRange } from './letter-range.type';
-export type { LetterRangeExclusion } from './letter-range-exclusion.type';
-export type { LetterRangeRepetition } from './letter-range-repetition.type';
-export type { LetterRangeRepetitionPattern } from './letter-range-repetition-pattern.type';
-export type { LetterRangeTuple } from './letter-range-tuple.type';
+export type {
+  LetterRange,
+  LetterRangeExclusion,
+  LetterRangeRepetition,
+  LetterRangeRepetitionPattern,
+  LetterRangeTuple
+} from './letter';
 
 // Lowercase.
-export type { LowercaseLetterRange } from './lowercase-letter-range.type';
-export type { LowercaseLetterRangePattern } from './lowercase-letter-range-pattern.type';
+export type { LowercaseLetterRange } from './lowercase/lib/lowercase-letter-range.type';
+export type { LowercaseLetterRangePattern } from './lowercase/lib/lowercase-letter-range-pattern.type';
 
 // Multi letter.
 export type { MultiLetterRange } from './multi-letter-range.type';
 export type { MultiLetterRangeRepetition } from './multi-letter-range-repetition.type';
 
 // Uppercase.
-export type { UppercaseLetterRange } from './uppercase-letter-range.type';
-export type { UppercaseLetterRangePattern } from './uppercase-letter-range-pattern.type';
+export type { UppercaseLetterRange } from './uppercase/lib/uppercase-letter-range.type';
+export type { UppercaseLetterRangePattern } from './uppercase/lib/uppercase-letter-range-pattern.type';
