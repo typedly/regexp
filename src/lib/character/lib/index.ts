16f652797e786471932ab700157b97a76bb0157d
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-02-08 13:02:23 +0000

    chore(character/index): update.

--- src/lib/character/lib/index.ts
+++ src/lib/character/lib/index.ts
@@ -1,2 +1,2 @@
-export type { CharacterRangeRepetition } from './character-range-repetition.type';
 export type { CharacterRange } from './character-range.type';
+export type { CharacterRangeRepetition } from './character-range-repetition.type';
