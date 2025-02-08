49c7b72f7768305be99aaf5ae65537979d83d56d
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-02-08 13:13:02 +0000

    refactor(LowercaseLetterRangePattern): use `Quantifier`.

--- src/lib/lowercase/lib/lowercase-letter-range-pattern.type.ts
+++ src/lib/lowercase/lib/lowercase-letter-range-pattern.type.ts
@@ -1,6 +1,7 @@
 // Type.
 import { LowercaseLetter } from '@typedly/letter';
 import { LowercaseLetterRange } from './lowercase-letter-range.type';
+import { Quantifier } from '../../quantifier.type';
 import { RegExpFlag } from '../../flag';
 import { RegExpPattern } from '../../regexp-pattern.type';
 /**
@@ -10,7 +11,7 @@ import { RegExpPattern } from '../../regexp-pattern.type';
  * @template {LowercaseLetter} [To='z'] - The ending letter in the range, defaults to 'z'.
  * @template {string} [Character=''] - An optional string of additional characters that will be escaped and appended to the range.
  * @template {boolean} [Negated=false] - A flag that indicates if the character class should be negated. If true, a caret (^) is added immediately after the opening bracket.
- * @template {number | '' | '*' | '+' | '?'} [Min=''] - The minimum repetition count (optional).
+ * @template {number | '' | Quantifier} [Min=''] - The minimum repetition count (optional).
  * @template {number | ''} [Max=''] - The maximum repetition count (optional).
  * @template {RegExpFlag[]} [Flags=[]] 
  */
@@ -19,7 +20,7 @@ export type LowercaseLetterRangePattern<
   To extends LowercaseLetter = 'z',
   Character extends string = '',
   Negated extends boolean = false,
-  Min extends number | '' | '*' | '+' | '?' = '',
+  Min extends number | '' | Quantifier = '',
   Max extends number | '' = '',
   Flags extends RegExpFlag[] = []
 > = RegExpPattern<LowercaseLetterRange<From, To, Character, Negated, Min, Max>, Flags>;
