563927c9d1fc25c73381453fed100bdf9bddf204
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-02-08 13:12:50 +0000

    refactor(LowercaseLetterRange): use `Quantifier`.

--- src/lib/lowercase/lib/lowercase-letter-range.type.ts
+++ src/lib/lowercase/lib/lowercase-letter-range.type.ts
@@ -2,6 +2,7 @@
 import { LetterRange } from '../../letter/lib';
 import { LowercaseLetter } from '@typedly/letter';
 import { Repetition } from '../../repetition.type';
+import { Quantifier } from '../../quantifier.type';
 /**
  * @description
  * @export
@@ -9,7 +10,7 @@ import { Repetition } from '../../repetition.type';
  * @template {LowercaseLetter} [To='z'] - The ending letter in the range, defaults to 'z'.
  * @template {string} [Character=''] - An optional string of additional characters that will be escaped and appended to the range.
  * @template {boolean} [Negated=false] - A flag that indicates if the character class should be negated. If true, a caret (^) is added immediately after the opening bracket.
- * @template {number | '' | '*' | '+' | '?'} [Min=''] - The minimum repetition count (optional).
+ * @template {number | '' | Quantifier} [Min=''] - The minimum repetition count (optional).
  * @template {number | ''} [Max=''] - The maximum repetition count (optional).
  */
 export type LowercaseLetterRange<
@@ -17,6 +18,6 @@ export type LowercaseLetterRange<
   To extends LowercaseLetter = 'z',
   Character extends string = '',
   Negated extends boolean = false,
-  Min extends number | '' | '*' | '+' | '?' = '',
+  Min extends number | '' | Quantifier = '',
   Max extends number | '' = ''
 > = `${LetterRange<From, To, Character, Negated>}${Repetition<Min, Max>}`;
