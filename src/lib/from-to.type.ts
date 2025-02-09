f9bbbeac7d83913bf0fb1f2a0ce06e46e6ef2c38
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-02-09 08:48:42 +0000

    refactor(FromTo): add `Negated` and `Delimiter`.

--- src/lib/from-to.type.ts
+++ src/lib/from-to.type.ts
@@ -1,10 +1,14 @@
 /**
  * @description
  * @export
- * @template {string | number} From 
- * @template {string | number} To 
+ * @template {string | number} [From=''] 
+ * @template {string | number} [To=''] 
+ * @template {boolean} [Negated=false] 
+ * @template {string} [Delimiter='-'] 
  */
 export type FromTo<
   From extends string | number = '',
-  To extends string | number = ''
-> = `${From}${From extends '' ? '' : To extends '' ? '' : '-'}${To}`;
+  To extends string | number = '',
+  Negated extends boolean = false,
+  Delimiter extends string = '-'
+> = `${Negated extends true ? '^' : ''}${From}${From extends '' ? '' : To extends '' ? '' : Delimiter}${To}`;
