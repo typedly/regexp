39c9a75f0b9b98ece34b1b041c45f328c0f4854b
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-02-12 02:37:07 +0000

    refactor(CharacterRange): include the array of string in the `Character` generic type variable.

--- src/lib/character/lib/character-range.type.ts
+++ src/lib/character/lib/character-range.type.ts
@@ -1,9 +1,14 @@
 // Type.
+import { EscapedStringFromArray } from "../../escaped-string-from-array.type";
 import { Escaped } from "../../escaped.type";
 import { FromTo } from "../../from-to.type";
 /**
  * @description
  * @export
+ * @template {string | number} [From='a'] 
+ * @template {string | number} [To='z'] 
+ * @template {string} [Character=''] 
+ * @template {boolean} [Negated=false] 
  * @example
  * const example1: CharacterRange<'a', 'z'>  = '[a-z]'; // "[a-z]"
  * const example2: CharacterRange<'', 'z'> = '[z]'; // "[z]"
@@ -11,14 +16,11 @@ import { FromTo } from "../../from-to.type";
  * const example4: CharacterRange<'', ''>  = '[]'; // "[]"
  * const example5: CharacterRange<'a', 'z', '_'> = '[a-z_]'; // "[a-z_]"
  * const example6: CharacterRange<'', '', '_'> = '[_]'; // "[_]"
- * @template {string | number} [From='a'] 
- * @template {string | number} [To='z'] 
- * @template {string} [Character=''] 
- * @template {boolean} [Negated=false] 
+ * const example7: CharacterRange<'a', 'z', ['d', 'w', 's']>  =  '[a-z\\d\\w\\s]'; // "[a-z\\d\\w\\s]"
  */
 export type CharacterRange<
   From extends string | number = 'a',
   To extends string | number = 'z',
-  Character extends string = '',
+  Character extends string | string[] = '',
   Negated extends boolean = false
-> = `[${FromTo<From, To, Negated, '-'>}${Escaped<Character>}]`;
+> = `[${FromTo<From, To, Negated, '-'>}${Character extends string ? Escaped<Character> : EscapedStringFromArray<Character>}]`;
